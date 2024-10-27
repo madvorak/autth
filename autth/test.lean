2aac53e13aa333071b96950df295b7b51c33f255
madvorak <martin.dvorak@matfyz.cz>
2024-10-27 11:42:08 +0100

    better syntax in test

--- autth/test.lean
+++ autth/test.lean
@@ -13,50 +13,38 @@ inductive alph where
 inductive nt where
   | S
 
-open alph
-open nt
 open Symbol
 open ContextFreeGrammar
 
-abbrev a_ : Symbol alph nt := terminal a
-abbrev b_ : Symbol alph nt := terminal b
-abbrev S_ : Symbol alph nt := nonterminal S
+abbrev a : Symbol alph nt := terminal alph.a
+abbrev b : Symbol alph nt := terminal alph.b
+abbrev S : Symbol alph nt := nonterminal nt.S
 
-def G : ContextFreeGrammar alph := ⟨nt, S, [
-    ⟨S, []⟩,
-    ⟨nt.S, [a_, S_, b_]⟩
+def G : ContextFreeGrammar alph := ⟨nt, nt.S, [
+    ⟨nt.S, []⟩,
+    ⟨nt.S, [a, S, b]⟩
   ]⟩
 
-example : [a,a,b,b] ∈ G.language := by
-  rw [mem_language_iff]
-  simp
-  rw [Derives]
-  apply Relation.ReflTransGen.tail
-  case b => exact [a_, a_, S_, b_, b_]
-  case a =>
-    apply Relation.ReflTransGen.tail
-    case b => exact [a_,S_,b_]
-    case a =>
-      apply Relation.ReflTransGen.tail
-      case b => exact [S_]
-      case a => rw [(by rfl : G.initial = S)]
-      case a =>
-        use ⟨nt.S, [a_, S_, b_]⟩
+example : [alph.a, alph.a, alph.b, alph.b] ∈ G.language := by
+  rw [mem_language_iff, List.map_cons, Derives]
+  apply Relation.ReflTransGen.tail (b := [a, a, S, b, b])
+  · apply Relation.ReflTransGen.tail (b := [a, S, b])
+    · apply Relation.ReflTransGen.tail (b := [S])
+      · rfl
+      · use ⟨nt.S, [a, S, b]⟩
         constructor
         · simp [G]
-        · apply (ContextFreeRule.rewrites_iff [S_] [a_, S_, b_]).mpr
+        · rw [ContextFreeRule.rewrites_iff]
           use []
           use []
           simp
-    case a =>
-      use ⟨nt.S, [a_, S_, b_]⟩
+    · use ⟨nt.S, [a, S, b]⟩
       constructor
       · simp [G]
-      · apply ContextFreeRule.Rewrites.cons (a_)
-        apply ContextFreeRule.Rewrites.head [b_]
-  case a =>
-    use ⟨nt.S, []⟩
+      · apply ContextFreeRule.Rewrites.cons a
+        apply ContextFreeRule.Rewrites.head [b]
+  · use ⟨nt.S, []⟩
     simp [G]
-    apply ContextFreeRule.Rewrites.cons (a_)
-    apply ContextFreeRule.Rewrites.cons (a_)
-    apply ContextFreeRule.Rewrites.head [b_, b_]
+    apply ContextFreeRule.Rewrites.cons a
+    apply ContextFreeRule.Rewrites.cons a
+    apply ContextFreeRule.Rewrites.head [b, b]
